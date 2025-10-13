823eb8ad4b27a6e0e12d2425e48cee085f7dc194
Jaime Mejia <jaimemejia1003@gmail.com>
2025-10-13 11:19:05 -0500

    Implement user management endpoints in UsersController
    - Added new endpoints for retrieving all users, finding users by ID and username, updating user details, and deleting users.
    - Introduced `createUserByAdmin` endpoint for admin user creation using `CreateUserAdminDto`.
    - Refactored existing methods to enhance user management functionality.

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	Post,
+	Body,
+	Get,
+	Param,
+	Patch,
+	Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserAdminDto } from './dtos/create-user-admin.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
-	// @Post('create')
-	// createUser(@Body() createUserDto: CreateUserDto) {
+	@Get('all')
+	findAllUsers() {
+		return this.usersService.findAllUsers();
+	}
 
-	// }
+	@Get('get-by-id')
+	findUserById(@Param('id') id: number) {
+		return this.usersService.findUserById(id);
+	}
+
+	@Get('get-by-username')
+	findUserByUsername(@Param('username') username: string) {
+		return this.usersService.findUserByUsername(username);
+	}
+
+	@Patch('update')
+	updateUser(@Body() updateUserDto: UpdateUserDto) {
+		return this.usersService.updateUser(updateUserDto);
+	}
+
+	@Delete('delete')
+	deleteUser(@Param('id') id: number) {
+		return this.usersService.deleteUser(id);
+	}
+
+	@Post('create-by-admin')
+	createUserByAdmin(@Body() createUserDto: CreateUserAdminDto) {
+		return this.usersService.createUserByAdmin(createUserDto);
+	}
 }
